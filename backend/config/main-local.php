8fe9aa1b65d0fa1958fde790b78f6ff33cb0f8ec
jmlemoine <jeanmelvinlp27@gmail.com>
2019-06-06 15:27:48 -0400

    adaptando el template admin al backend

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -2,10 +2,20 @@
 
 $config = [
     'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                   '@app/views' => '@backend/themes/admin'
+                   //'@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                   //'@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/testing/app'
+                ],
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'CdCYxgtbIfFPkT82BuUYwgPkK7siqKfN',
         ],
+
     ],
 ];
 
